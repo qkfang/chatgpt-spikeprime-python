2fd372389ceec292f67942735d5ed6ada99a06d4
Daniel Fang <qkfang@hotmail.com>
2024-05-20 22:02:04 +1000

    add logics

--- doc-v2/lego-api-parse.py
+++ doc-v2/lego-api-parse.py
@@ -7,3 +7,24 @@ with open("lego-doc-api.html",  encoding='utf-8') as fp:
 for br in soup.find_all("br"):
     br.replace_with("\n")
     
+
+    
+apiSpec = []
+for moduleNode_h3 in soup.findAll("h3"):
+    print('--------------------')
+
+    module = {}
+    apiSpec.append(module)
+
+    module['Module_Name'] = moduleNode_h3.text.strip() # h3
+    module['Module_Description'] = moduleNode_h3.find_next_sibling('div').text.strip()
+    module['SubModules'] = []
+    module['Code_Import'] = []
+
+
+
+
+with open("lego-api-sp.json", "w") as text_file:
+    text_file.write(json.dumps(apiSpec, indent=2))
+
+# print(soup)
