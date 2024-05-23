54b88af1db86a5480fb94d1b84240d927111ffd3
Daniel Fang <qkfang@hotmail.com>
2024-05-23 23:23:41 +1000

    loop

--- doc-v2/lego-api-parse.py
+++ doc-v2/lego-api-parse.py
@@ -45,6 +45,66 @@ for moduleNode_h3 in soup.findAll("h3"):
                 code.unwrap()
 
             codeBlock['Python'] = moduleNode_h3_code.text
+
+
+            
+    for subModelNode_a in (moduleNode_h3.parent).findAll(
+                        lambda tag:tag.name == "a" and 
+                        len(tag.attrs) == 1 and
+                        tag.attrs['id'].startswith("lls-help-python-spm") and
+                        "section-header" not in tag.attrs['id']
+                    ):
+        
+
+        if(subModelNode_a.find_next_sibling('h4') is None):
+            continue
+
+        subModule = {}
+        module['SubModules'].append(subModule)
+
+        specSubNode_h4 = subModelNode_a.find_next_sibling('h4')
+        subModule['SubModel_Name'] = specSubNode_h4.text.strip()
+        subModule['SubModel_Description'] = specSubNode_h4.find_next_sibling('div').text.strip()
+        subModule['SubModel_Description']
+        subModule['Functions'] = []
+        subModule['Code_Snippet'] = []
+
+        for funcNode_h5 in (specSubNode_h4.parent).findAll(
+                    lambda tag:tag.name == "h5"
+                ):
+            
+            if(funcNode_h5.find_next_sibling('div') is None):
+                continue
+            
+            if(funcNode_h5.text.strip() == 'Parameters'):
+                continue
+            
+            function = {}
+            subModule['Functions'].append(function)
+
+            function['Function_Name'] = funcNode_h5.text.strip() # h5
+            function['Function_Signature'] = funcNode_h5.find_next_sibling('div').text.strip()
+            function['Parameters'] = []
+
+            for paramNode_h6 in (funcNode_h5.parent).findAll(
+                        lambda tag:tag.name == "h6"
+                    ):
+
+                print(paramNode_h6)
+                if(paramNode_h6.find_next_sibling('div') is None):
+                    continue
+                
+                if(paramNode_h6.text.strip() == 'Parameters'):
+                    continue
+
+                if(paramNode_h6.text.strip() == 'Constants'):
+                    continue
+
+                funcParam = {}
+                funcParam['Parameter_Name'] = paramNode_h6.text.strip()
+                funcParam['Parameter_Description'] = paramNode_h6.find_next_sibling('div').text.strip()
+                function['Parameters'].append(funcParam)
+
         
 
 
